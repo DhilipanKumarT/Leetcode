44ebffe7795b3eea17c1c6a440290daa6e23b293
DHILIPAN KUMAR T <166507239+DhilipanKumarT@users.noreply.github.com>
2024-08-17 14:40:45 +0530

    Time: 162 ms (28.84%) | Memory: 45.8 MB (5.22%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -2,9 +2,11 @@ class Solution
 {
   public String shortestPalindrome(String s) 
   {
-    StringBuilder sb= new StringBuilder(s);
-    sb.reverse();
-    String res=sb.toString();
+    String res="";
+    for(int i=s.length()-1;i>=0;i--)
+    {
+        res=res+Character.toString((s.charAt(i)));
+    }
     for (int i = 0; i < res.length(); i++)
     {
       if (s.startsWith(res.substring(i)))
