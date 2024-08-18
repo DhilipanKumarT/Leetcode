8cd0575f65dc4068168347e7d2bc5ebf5414416b
DHILIPAN KUMAR T <166507239+DhilipanKumarT@users.noreply.github.com>
2024-08-18 23:13:19 +0530

    Time: 5 ms (25.27%) | Memory: 42.3 MB (27.25%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String truncateSentence(String s, int k)
+    {
+        String str[]=s.split(" ");
+        String res="";
+        for(int i=0;i<k;i++)
+        {
+            res=res+str[i]+" ";
+        }
+     return res.trim();   
+    }
+}
