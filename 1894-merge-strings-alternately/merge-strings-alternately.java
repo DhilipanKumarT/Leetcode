2c7e807d883657d76c6f3b6e15819d68c202429b
DHILIPAN KUMAR T <166507239+DhilipanKumarT@users.noreply.github.com>
2024-08-19 15:54:44 +0530

    Time: 7 ms (22.09%) | Memory: 42.7 MB (19.32%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,19 +1,22 @@
-class Solution {
-   public String mergeAlternately(String word1, String word2) {
-       
-       String result = "";
-       int len1 = word1.length();
-       int len2 = word2.length();
-       int maxLength = Math.max(len1, len2);
-
-       for (int i = 0; i < maxLength; i++) {
-           if (i < len1) {
-               result += word1.charAt(i);
-           }
-           if (i < len2) {
-               result += word2.charAt(i);
-           }
-       }
-       return result;
+class Solution 
+{
+   public String mergeAlternately(String word1, String word2) 
+   {
+    String res="";
+    int l1=word1.length();
+    int l2=word2.length();
+    int max=Math.max(l1,l2);
+    for(int i=0;i<max;i++)
+    {
+        if(i<l1)
+        {
+            res+=word1.charAt(i);
+        }
+        if(i<l2)
+        {
+            res+=word2.charAt(i);
+        }
+    } 
+    return res;
    }
 }
