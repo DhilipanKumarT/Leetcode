a71748bdca3133af49e0d1dab6f49e177d2ed0e3
DHILIPAN KUMAR T <166507239+DhilipanKumarT@users.noreply.github.com>
2024-08-22 22:21:15 +0530

    Time: 3 ms (99.68%) | Memory: 68.4 MB (44.15%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,36 @@
+class Solution
+{
+    public boolean isPalindrome(ListNode head)
+    {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+       ListNode p=slow;
+       ListNode q=null;
+       ListNode r=null;
+        while(p!=null)
+        {
+            r=q;
+            q=p;
+            p=p.next;
+            q.next=r;
+        }
+        while(q!=null)
+        {
+            if(head.val==q.val)
+            {
+                q=q.next;
+                head=head.next;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    return true;
+    }
+}
